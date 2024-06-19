a787c2b52d7c5de92cec08b820bc233895914061
LAPTOP-M7T10733\jub12 <jub1234jub@gmail.com>
2024-06-19 18:20:52 +0300

    Init

---
+++ BiblicallyAccurateLasers/ModClass.cs
@@ -0,0 +1,34 @@
+﻿using Modding;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UObject = UnityEngine.Object;
+
+namespace BiblicallyAccurateLasers
+{
+    public class BiblicallyAccurateLasers : Mod
+    {
+        internal static BiblicallyAccurateLasers Instance;
+
+        //public override List<ValueTuple<string, string>> GetPreloadNames()
+        //{
+        //    return new List<ValueTuple<string, string>>
+        //    {
+        //        new ValueTuple<string, string>("White_Palace_18", "White Palace Fly")
+        //    };
+        //}
+
+        public BiblicallyAccurateLasers() : base("Biblically Accurate Lasers") { }
+        public override string GetVersion() => "0.0.0";
+
+        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
+        {
+            Log("Initializing");
+
+            Instance = this;
+
+            Log("Initialized");
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+# Biblically Accurate Lasers
+A resurrection of an old idea, expanded upon. Adds some lasers to make the Radiance fight a bit more challenging.
+
+Beauty is in the eye of the beholder.
+She saw all, and yet she saw nothing: for her own light blinded seventeen of her eyes.
