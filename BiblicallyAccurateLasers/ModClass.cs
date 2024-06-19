d77fb6a4b6c9026422acbb22b42fd3992bebe7bb
LAPTOP-M7T10733\jub12 <jub1234jub@gmail.com>
2024-06-19 22:28:58 +0300

    Added crown

--- BiblicallyAccurateLasers/ModClass.cs
+++ BiblicallyAccurateLasers/ModClass.cs
@@ -41,15 +41,48 @@ namespace BiblicallyAccurateLasers
         {
             if (enemy.name.Contains("Radiance"))
             {
-                GameObject eye = new GameObject();
-                eye.AddComponent<SpriteRenderer>();
-                eye.GetComponent<SpriteRenderer>().sprite = BiblicallyAccurateLasers.GetSprite(TextureStrings.EyeKey);
-                eye.SetActive(true);
-                eye.transform.parent = enemy.transform;
-                eye.transform.localPosition = new Vector3(-0.1f, 1.5f, 0f);
+                GameObject crown = Crown(enemy.transform, new Vector3(-0.1f, 2f, 0f), Vector3.one);
             }
 
             return isAlreadyDead;
+        }
+
+        private GameObject Crown(Transform parent, Vector3 localPosition, Vector3 localScale, int eyes = 7)
+        {
+            GameObject crown = new();
+
+            GameObject eyesHolder = new();
+            eyesHolder.transform.parent = crown.transform;
+            eyesHolder.transform.localPosition = Vector3.zero;
+            eyesHolder.transform.localScale = Vector3.one;
+            eyesHolder.transform.localRotation = Quaternion.Euler(0, 0, 0);
+
+            for (int i = 0; i < eyes; i++)
+            {
+                GameObject eyeCarrier = new();
+                eyeCarrier.transform.parent = eyesHolder.transform;
+                eyeCarrier.transform.localPosition = Vector3.zero;
+                eyeCarrier.transform.localScale = Vector3.one;
+
+                GameObject eye = new();
+                eye.AddComponent<SpriteRenderer>();
+                eye.GetComponent<SpriteRenderer>().sprite = BiblicallyAccurateLasers.GetSprite(TextureStrings.EyeKey);
+                eye.SetActive(true);
+                eye.transform.parent = eyeCarrier.transform;
+                eye.transform.localPosition = new Vector3(0, 3f, 0);
+                eye.transform.localScale = Vector3.one * 0.1f;
+
+                eyeCarrier.transform.rotation = Quaternion.Euler(0, 0, i * 360f / eyes);
+                eye.transform.localEulerAngles = new Vector3(-90f, 0, 0);
+            }
+            eyesHolder.AddComponent<Spin>();
+
+            crown.transform.parent = parent.transform;
+            crown.transform.localPosition = localPosition;
+            crown.transform.localScale = localScale;
+            crown.transform.rotation = Quaternion.Euler(90, 0, 0);
+
+            return crown;
         }
     }
 }
---
+++ BiblicallyAccurateLasers/Spin.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace BiblicallyAccurateLasers
+{
+    internal class Spin : MonoBehaviour
+    {
+
+        private float rot;
+        private float speed = 1f;
+
+        public void Start()
+        {
+            rot = transform.localRotation.eulerAngles.z;
+        }
+
+        public void Update()
+        {
+            rot += speed * Time.timeScale;
+            transform.localRotation = Quaternion.Euler(0, 0, rot);
+        }
+
+        public void SetSpeed(float s)
+        {
+            speed = s;
+        }
+    }
+}
